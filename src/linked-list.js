ddcee5020b50f3f73fad52e4e69f4676e8dc2e15
lesia262 <f.lesenka@gmail.com>
2017-02-03 20:54:13 +0300

    append tail head

--- src/linked-list.js
+++ src/linked-list.js
@@ -3,15 +3,42 @@ const Node = require('./node');
 class LinkedList {
     constructor() {
         this.length = 0;
-        this.head = null;
-        this.tail = null;
+        this._head = null;
+        this._tail = null;
     }
 
-    append(data) {}
+    append(data) {
+        var node1 = new Node();
+        var node2 = new Node();
 
-    head() {}
+        if(this.length == 0){
+            this.length = 1;
+            this._head = node1;
+            this._tail = node1;
+            node1.data = data;
+            return;
+            }
 
-    tail() {}
+        node2 = this._tail;
+        node1.prev = node2;
+        node2.next = node1;
+        node1.data = data;
+        this._tail = node1;
+        this.length++;
+        return;
+    }
+
+    head() {
+        var node1 = new Node();
+        node1 = this._head;
+        return node1.data;
+    }
+
+    tail() {
+        var node40 = new Node();
+        node40 = this._tail;
+        return node40.data;
+    }
 
     at(index) {}
 
@@ -29,6 +56,3 @@ class LinkedList {
 }
 
 module.exports = LinkedList;
-
-var MyFirst = new LinkedList (); 
-MyFirst.head();
